1b4b2609df97cde1f3afd1ddd32af2ecda590b9e
jian-ferreira <jianferreiradev@gmail.com>
2023-05-17 21:17:04 -0300

    First commit the of project Ruby Shop.

---
+++ src/main/java/com/ruby/RubyLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.ruby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyLojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubyLojaVirtualApplication.class, args);
+	}
+
+}
