bb96ae0aca15557b9f3b138c8fe9b11637fdb144
jian-ferreira <jianferreiradev@gmail.com>
2023-05-22 21:09:52 -0300

    Criado a classe de Marca Produto

---
+++ src/main/java/com/ruby/model/MarcaProduto.java
@@ -0,0 +1,42 @@
+package com.ruby.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	private Long id;
+	@Column
+	private String nomeDaDescricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nomeDaDescricao;
+	}
+
+	public void setNome(String nome) {
+		this.nomeDaDescricao = nome;
+	}
+
+}
