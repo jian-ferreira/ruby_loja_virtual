ec7e3662455fe7a852501094e7dd00e019ed89c8
jian-ferreira <jianferreiradev@gmail.com>
2023-05-22 21:50:26 -0300

    Commit da classe Categoria Produto.

--- src/main/java/com/ruby/model/CategoriaProduto.java
+++ src/main/java/com/ruby/model/CategoriaProduto.java
@@ -12,32 +12,32 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	@Column
 	private String nomeDaDescricao;
 
-	public Long getId() {
+	public Long getIdCAtegoriaProduto() {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setIdCAtegoriaProduto(Long idCAtegoriaProduto) {
+		this.id = idCAtegoriaProduto;
 	}
 
-	public String getNome() {
+	public String getNomeDaDescricao() {
 		return nomeDaDescricao;
 	}
 
-	public void setNome(String nome) {
-		this.nomeDaDescricao = nome;
+	public void setNomeDaDescricao(String nomeDaDescricao) {
+		this.nomeDaDescricao = nomeDaDescricao;
 	}
 
 	@Override
@@ -53,10 +53,8 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	 
 
 }
